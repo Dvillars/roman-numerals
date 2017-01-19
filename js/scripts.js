f48567ce431145c7120bf72a43d7adad68a2c062
Derek Villars and Sam Kirsch <sam@>
2017-01-19 10:09:20 -0800

    create numberEvaluate function to determine which numerals to kick out

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var romans = ['M','D','C','L','X','V','I'];
+var romans10 = ['I','X','C','M'];
+var romans5 = ['V','L','D']
 
 var romanChecker = function(textInput){
   var result;
@@ -12,15 +13,45 @@ var romanChecker = function(textInput){
   return result;
 };
 
+var numberSplit = function(textInput) {
+  var numberArray = textInput.split("");
+  return numberArray;
+}
+
+var numberEvaluate = function(number, position) {
+  var numeral = "";
+  if (number < 4) {
+    console.log("It's less than four");
+    numeral += romans10[position].repeat(number);
+  } else if (number === 4) {
+    console.log("it's four!");
+    numeral = romans10[position] + romans5[position];
+  } else if (number < 9 && number > 4) {
+    console.log("It's less than 9");
+    numeral += romans5[position];
+    number = number % 5;
+    numeral += romans10[position].repeat(number);
+  } else if (number === 9) {
+    console.log("It's 9");
+    numeral = romans10[position] + romans10[position+1];
+  } else {
+    console.log('OHNO');
+  };
+  return numeral;
+}
+
 $(function(){
   $("form.roman-form").submit(function(event) {
     event.preventDefault();
     var textInput = $("#text-input").val();
     var isValid = romanChecker(textInput);
+    var textOutput;
       if (isValid === true) {
-        var textOutput = numberSplit(textInput);
+        var numberArray = numberSplit(textInput);
+        textOutput = numberEvaluate(parseInt(numberArray[0]), 0);
+        console.log(textOutput);
       } else {
-        var textOutput = 'NOT A VALID NUMBA!!!!'
+        textOutput = 'NOT A VALID NUMBA!!!!'
       }
     $(".results").text(textOutput);
   })
