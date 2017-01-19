cc0c63c8daf1c044b6baf4120d18fdbb398b9b49
Derek Villars and Sam Kirsch <derek@>
2017-01-19 09:18:40 -0800

    add romanChecker function and refactor output

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
+var romans = ['M','D','C','L','X','V','I'];
 
+var romanChecker = function(textInput){
+  var result;
 
+  if (parseInt(textInput) <= 3999){
+    result = true;
+  } else {
+    result = false;
+  };
+
+  return result;
+};
 
 $(function(){
   $("form.roman-form").submit(function(event) {
     event.preventDefault();
-    var output = $("#text-input").val();
-    $(".results").text(output);
+    var textInput = $("#text-input").val();
+    var isValid = romanChecker(textInput);
+      if (isValid === true) {
+        var textOutput = numberSplit(textInput);
+      } else {
+        var textOutput = 'NOT A VALID NUMBA!!!!'
+      }
+    $(".results").text(textOutput);
   })
 });
