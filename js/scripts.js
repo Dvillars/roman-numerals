2fa78a49ef46729016939a9bf5d6201de5c34ceb
Derek Villars and Sam Kirsch <sam@>
2017-01-19 11:42:01 -0800

    compress and compact the code into a single large function

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,41 @@
-var romans10 = ['I','X','C','M'];
-var romans5 = ['V','L','D']
+var romanTranslate = function(textInput) {
+  var romans10 = ['I','X','C','M'];
+  var romans5 = ['V','L','D']
 
-var romanChecker = function(textInput){
-  var result;
+  var numberEvaluate = function(number, position) {
+    var numeral = "";
+    if (number < 4) {
+      numeral += romans10[position].repeat(number);
+    } else if (number === 4) {
+      numeral = romans10[position] + romans5[position];
+    } else if (number < 9 && number > 4) {
+      numeral += (romans5[position] + romans10[position].repeat(number-5));
+    } else if (number === 9) {
+      numeral = romans10[position] + romans10[position+1];
+    } else {
+    };
+    return numeral;
+  }
 
-  if (parseInt(textInput) <= 3999){
-    result = true;
+  if (parseInt(textInput) <= 3999 && /\d+/.test(textInput) === true && /\./.test(textInput) === false) {
+    var revNumArray = textInput.split("").reverse();
+    var outputNumeral = [];
+    for (i=0; i < revNumArray.length; i++) {
+      var number = parseInt(revNumArray[i]);
+      outputNumeral.push(numberEvaluate(parseInt(revNumArray[i]), i));
+    };
+    var finalNumeral = outputNumeral.reverse().join("");
+    return finalNumeral;
   } else {
-    result = false;
-  };
-
-  return result;
-};
-
-var numberSplit = function(textInput) {
-  var numberArray = textInput.split("");
-  return numberArray;
-}
-
-var numberEvaluate = function(number, position) {
-  var numeral = "";
-  if (number < 4) {
-    console.log("It's less than four");
-    numeral += romans10[position].repeat(number);
-  } else if (number === 4) {
-    console.log("it's four!");
-    numeral = romans10[position] + romans5[position];
-  } else if (number < 9 && number > 4) {
-    console.log("It's less than 9");
-    numeral += romans5[position];
-    number = number % 5;
-    numeral += romans10[position].repeat(number);
-  } else if (number === 9) {
-    console.log("It's 9");
-    numeral = romans10[position] + romans10[position+1];
-  } else {
-    console.log('OHNO');
-  };
-  return numeral;
-}
-
-var romanTranslate = function(numberArray) {
-  var revNumArray = numberArray.reverse();
-  var outputNumeral = [];
-  for (i=0; i < revNumArray.length; i++) {
-    var number = parseInt(revNumArray[i]);
-    outputNumeral.push(numberEvaluate(number, i));
-  };
-  var finalNumeral = outputNumeral.reverse().join("");
-  return finalNumeral;
+      var textOutput = 'NOT A VALID NUMBA!!!!'
+  }
 };
 
 $(function(){
   $("form.roman-form").submit(function(event) {
     event.preventDefault();
     var textInput = $("#text-input").val();
-    var isValid = romanChecker(textInput);
-    var textOutput;
-      if (isValid === true) {
-        var numberArray = numberSplit(textInput);
-        textOutput = romanTranslate(numberArray);
-
-      } else {
-        textOutput = 'NOT A VALID NUMBA!!!!'
-      }
+    var textOutput = romanTranslate(textInput);
     $(".result").text(textOutput).slideDown();
   })
 });
